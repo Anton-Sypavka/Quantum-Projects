1cc7fed4f7b6bbaf8864f8f12b5ed035e7598c11
Anton Sypavka <anton.sypavka@gmail.com>
2021-02-26 01:36:13 +0200

    Quantum-projects: Form with logic done.

--- js/script.js
+++ js/script.js
@@ -63,4 +63,91 @@ document.addEventListener('DOMContentLoaded', function () {
 
     addItemNumber();
 
+    // /**
+    //  * @desc Function that adds order number to the current slide.
+    //  **/
+
+    const questionList = document.querySelector('.questions-list');
+    questionList.addEventListener('click', openQuestionTab);
+
+    function openQuestionTab(event) {
+        event.preventDefault();
+        const {target} = event;
+
+        if (target.classList.contains('fa-plus') || target.classList.contains('fa-times')) {
+            const parent = event.target.closest('.questions-list__item');
+            parent.lastElementChild.classList.toggle('visible');
+
+            target.classList.toggle('fa-plus');
+            target.classList.toggle('fa-times');
+        }
+    }
+
+
+    // const email = document.querySelector('.input-email');
+    // const name = document.querySelector('.input-name');
+    //
+    // const namePattern = /[A-Za-z]/;
+    // const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    //
+    //
+    // name.addEventListener('change', function (value) {
+    //     if (name.value.match(namePattern)) {
+    //         console.log('Good!')
+    //         name.classList.add('valid')
+    //     } else {
+    //         console.log('Bad')
+    //     }
+    // });
+    //
+    // email.addEventListener('keydown', function (value) {
+    //     if (email.value.match(emailPattern)) {
+    //         email.classList.add('valid')
+    //         console.log('Good!')
+    //     } else {
+    //         console.log('Bad')
+    //     }
+    // });
+    //
+    //
+    //
+    //
+
+
+
+
 })
+
+
+function validateName(value) {
+    const nameError = document.querySelector('.name-error');
+    const namePattern = /[A-Za-z]/;
+
+    !value.match(namePattern)
+        ? nameError.innerHTML = 'incorrect'
+        : nameError.innerHTML = ''
+}
+
+function validateEmail(value) {
+    const emailError = document.querySelector('.email-error');
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    !value.match(emailPattern)
+        ? emailError.innerHTML = 'incorrect'
+        : emailError.innerHTML = ''
+}
+
+function submitButtonEnabled () {
+    const name = document.querySelector('.input-name').value
+    const email = document.querySelector('.input-email').value
+    const submitButton = document.querySelector('.submit-button')
+    const namePattern = /[A-Za-z]/;
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if (name.match(namePattern) && email.match(emailPattern)) {
+        submitButton.removeAttribute('disabled');
+    }
+}
+
+
+
